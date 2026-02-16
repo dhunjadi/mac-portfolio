98d4a0131aae951e2a7786054f7264754ffe11c4
Dario Hunjadi <dario.hunjadi@hotmail.com>
2026-02-16 19:59:05 +0100

    resizing the dock based on the number of icons

--- src/components/Dock.tsx
+++ src/components/Dock.tsx
@@ -1,19 +1,21 @@
-import { faApple } from "@fortawesome/free-brands-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import type { CSSProperties } from "react";
+import appleLogo from "/icons/apple-logo.svg";
 
 type DockProps = {
   isVisible?: boolean;
 };
 
 const Dock = ({ isVisible }: DockProps) => {
+  const icons = [appleLogo, appleLogo, appleLogo, appleLogo];
+  const dockStyle = {
+    "--dock-items": icons.length,
+  } as CSSProperties;
+
   return (
-    <div className={`c-dock ${isVisible ? "" : "hidden"}`}>
-      <div className="c-dock__container">
-        <FontAwesomeIcon icon={faApple} size="5x" color="white" />
-        <FontAwesomeIcon icon={faApple} size="5x" color="white" />
-        <FontAwesomeIcon icon={faApple} size="5x" color="white" />
-        <FontAwesomeIcon icon={faApple} size="5x" color="white" />
-      </div>
+    <div className={`c-dock ${isVisible ? "" : "hidden"}`} style={dockStyle}>
+      {icons.map((icon, index) => (
+        <img src={icon} alt="" key={index} />
+      ))}
     </div>
   );
 };
