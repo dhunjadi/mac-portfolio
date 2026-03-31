366bb7b38d47f679617163719cc57573f35b6604
Dario Hunjadi <dario.hunjadi@hotmail.com>
2026-03-31 20:39:38 +0200

    Updates

--- assets/BrightnessOverlay-BlJSM93B.js
+++
@@ -1,1 +0,0 @@
-import{J as t,j as e}from"./index-ROHcEh9Z.js";const r=.65,a=()=>{const s=(1-t()/100)*r;return e.jsx("div",{className:"c-brightnessOverlay","aria-hidden":!0,style:{"--brightness-dim":s.toString()}})};export{a as default};
