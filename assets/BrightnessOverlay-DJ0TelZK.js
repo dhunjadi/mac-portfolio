aefba9ddbf6db4b14f8126168f174de5389d7b57
Dario Hunjadi <dario.hunjadi@hotmail.com>
2026-04-02 21:54:40 +0200

    Updates

---
+++ assets/BrightnessOverlay-DJ0TelZK.js
@@ -0,0 +1,1 @@
+import{N as t,j as e}from"./index-DAZC9soE.js";const r=.65,a=()=>{const s=(1-t()/100)*r;return e.jsx("div",{className:"c-brightnessOverlay","aria-hidden":!0,style:{"--brightness-dim":s.toString()}})};export{a as default};
